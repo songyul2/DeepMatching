0a9666ea8dac978f5de00248a0d7d5568c98950f
Songyu Liu <songyul4@odin.ics.uci.edu>
2020-11-17 20:08:34 -0800

    script updated

--- run_program.sh
+++ run_program.sh
@@ -7,15 +7,16 @@ do
 filename=$(basename "$file")
 output="./data/align_${filename}_history"
 rm -f ${output}
-	for ((i=15; i<=100; i+=5));
+	for ((i=68; i<=100; i+=2));
 	do
 		nodes="$i"
-
+		{
 		# the first input is syeast0
 		python DeepMatching.py --input1 ./networks/syeast0/syeast0.el  \
 		--input2 ${file}	\
-		--nodes ${nodes} --propa_num $((4*nodes))	\
-		# >> ${output}
-		# add new lines to separate 
+		--nodes ${nodes} --propa_num $((4*nodes))	
+		echo
+		echo '#####'
+		}>> ${output}
 	done
 done
